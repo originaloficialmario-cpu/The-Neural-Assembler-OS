b3f8ebb34cdae90e4d24fb4b812dbece9c11bb0c
Supra_Mario <original.oficial.mario@gmail.com>
2026-06-17 03:29:34 +0200

    feat: Add Multiboot-compliant bootloader (boot.asm) for bare-metal execution.

--- boot.asm
+++ boot.asm
@@ -5,29 +5,23 @@ section .multiboot
 align 4
 dd 0x1BADB002             ; Die Magic Number: Sagt der Hardware "Ich bin ein OS!"
 dd 0x00                   ; Flags
-dd - (0x1BADB002 + 0x00)  ; Checksumme (Beweist, dass die Magic Number fehlerfrei ist)
+dd - (0x1BADB002 + 0x00)  ; Checksumme
 
-; 2. Etwas Platz im Speicher (RAM) für den Anfang reservieren
+; 2. Etwas Platz im Speicher (RAM) reservieren
 section .bss
 align 16
 stack_bottom:
-resb 4096                 ; 4 Kilobyte Speicher für Notizen der CPU
+resb 4096                 ; 4 KB Speicher
 stack_top:
 
 ; 3. Der eigentliche Start-Code
 section .text
 global start
-extern _start             ; Das ist der Link zu deiner Rust-Datei (main.rs)
+extern _start             ; Link zu main.rs
 
 start:
-    ; Dem Prozessor sagen, wo er seine 4 KB Speicher findet
     mov esp, stack_top
-
-    ; JETZT GEHT'S LOS: Übergabe an deinen Rust-Kernel!
-    call _start
-
-    ; Falls dein Rust-Code sich jemals beenden sollte, 
-    ; lassen wir den Prozessor hier anhalten, damit er nicht abstürzt.
+    call _start           ; Übergabe an den Rust-Kernel!
     cli
 .hang:
     hlt
